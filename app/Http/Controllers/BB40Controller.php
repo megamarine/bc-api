91d633b2ba176e3ba95bc9aef9b9c9d02464d2bd
Firman Firdaus <firmanfirdaus542@gmail.com>
2023-10-12 16:42:32 +0700

    update data maping BC 40 dan query model

--- app/Http/Controllers/BB40Controller.php
+++ app/Http/Controllers/BB40Controller.php
@@ -12,14 +12,54 @@ class BB40Controller extends Controller
      */
     public function index()
     {
-        $data = BB40::where('jenis','BC 4.0')->orderBy('datedoc','desc')->get();
-        return response()->json([
-            'status' => true,
-            'message' => 'Data BC 4.0 ditemukan',
-            'asalData' => 'S',
-            'asuransi' => '0.00',
-            'data' => $data
-        ],200);
+        // BB40::where('jenis','BC 4.0')->orderBy('datedoc','desc')->get();
+        $data = new BB40();
+        $data2 = $data->bcbb40();
+        
+        foreach ($data2 as $row) {
+            $harga_penyerahan = $row->harga_po * $row->qty_flow;
+            $kode_item = substr($row->kode_item,0,1); 
+            if ($kode_item == 9) {
+                $tujuan_kirim = '2';
+            }else {
+                $tujuan_kirim = '1';
+            }
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Data BC 4.0 ditemukan',
+                'asalData' => 'S',
+                'asuransi' => number_format(0, 2),
+                'bruto' => $row->qty,
+                'cif' => 0.00,
+                'kodeJenisTpb' => 1,
+                'freight' => number_format(0, 2),
+                'hargaPenyerahan' => $harga_penyerahan,
+                'idPengguna' => '-',
+                'jabatanTtd' => 'KUASA DIREKSI',
+                'jumlahKontainer' => 0,
+                'jumlahKontainer' => '40',
+                'kodeKantor' => '071300',
+                'kodeTujuanPengiriman' => $tujuan_kirim,
+                'kotaTtd' => 'PASURUAN',
+                'namaTtd' => 'ADI SURYA',
+                'netto' => $row->qty,
+                'nik' => '017185901651000',
+                'nomorAju' => 'waiting list ...',
+                'seri' => 0,
+                'tanggalAju' => date('Y-m-d', strtotime($row->DATE)),
+                'tanggalTtd' => date('Y-m-d', strtotime($row->DATE)), 
+                'volume' => 'Waiting List ..',
+                'biayaTambahan' => 0.00,
+                'biayaPengurang' => 0.00,
+                'vd' => 0.00,
+                'uangMuka' => 0.00,
+                'nilaiJasa' => 0.00,
+                'entitas' => array(
+                    'property' => $row->docp
+                )
+            ],200);
+        }
     }
 
     /**
--- app/Models/BB40.php
+++ app/Models/BB40.php
@@ -4,10 +4,50 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class BB40 extends Model
 {
     use HasFactory;
 
     protected $table = 'bcmasuk';
+
+    function bcbb40()  {
+        return DB::connection('mysql')->select("SELECT
+            a.docp,
+            a.DATE,
+            a.jnsp,
+            a.oms as no_po,
+            b.nodoc as penerimaan_barang,
+            c.sub as kode_supp, 
+            c.remark as ket_kebutuhan, 
+            c.nosj as sj_supp, 
+            c.nodoc as doc_bc,
+            d.inv as kode_item, 
+            d.loc, 
+            d.spesifikasi, 
+            d.qty, 
+            d.no, 
+            d.unit, 
+            d.qty1, 
+            d.cct, 
+            d.qtygd as qty_flow, 
+            d.etd, 
+            d.unitgd, 
+            d.qtytim,
+            e.name as nama_supp,
+            e.address,
+            e.city,
+            f.name as item_name,
+            g.price as harga_po
+        FROM
+            docp a 
+        JOIN docpd b on b.docp = a.docp
+        JOIN spb c on c.spb = b.nodoc
+        JOIN spbd d on d.spb = b.nodoc
+        JOIN sub e on e.sub = c.sub
+        JOIN inv f on f.inv = d.inv
+        JOIN omd g on g.oms = a.oms
+        WHERE a.jnsp = 'BC 4.0' and b.nodoc = 'SPB-2310-000069' ORDER BY date DESC");
+    }
 }
