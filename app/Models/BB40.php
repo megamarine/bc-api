d3a3f105f240a61389b0647652c9e1682fd45202
Firman Firdaus <firmanfirdaus542@gmail.com>
2023-11-03 14:10:10 +0700

    update query dan format mapping bc 40

--- app/Models/BB40.php
+++ app/Models/BB40.php
@@ -51,7 +51,71 @@ class BB40 extends Model
         JOIN sub e on e.sub = c.sub
         JOIN inv f on f.inv = d.inv
         JOIN omd g on g.oms = a.oms
-        WHERE a.jnsp = 'BC 4.0' ORDER BY date DESC");
+        WHERE a.jnsp = 'BC 4.0' and b.nodoc = 'SPB-2310-000138' ORDER BY date DESC");
     }
     // and b.nodoc = 'SPB-2310-000069'
+    // SPB-2310-000138
+
+    function get_data_bb40() {
+        return DB::connection('mysql')->select("SELECT
+            a.docp,
+            a.`date` as date_bc,
+            a.jnsp,
+            a.oms,
+            b.nodoc as spb,
+            c.sub as kode_supp,
+            c.remark as ket_spb,
+            c.nopol,
+            c.nosj,
+            c.nodoc as nopen,
+            c.date as date_spb,
+            d.name as nama_supp,
+            d.address,
+            d.city
+        FROM docp a
+            JOIN docpd b on b.docp = a.docp
+            JOIN spb c on c.spb = b.nodoc
+            JOIN sub d ON d.sub = c.sub
+        WHERE
+            a.jnsp = 'BC 4.0'
+            and b.nodoc = 'SPB-2310-000138'
+        ORDER BY a.`date` DESC");
+        
+    }
+
+    function get_detail_data_spb($spb) {
+        return DB::connection('mysql')->select("SELECT
+            g.inv,
+            g.loc as gd_spb,
+            g.remark as nama_item,
+            g.spesifikasi,
+            g.qty,
+            g.qtygd as qty_flow,
+            g.unitgd,
+            g.cct,
+            h.oms as po_from_spb,
+            i.price as unit_price
+        from spbd g
+        JOIN spb h ON h.spb = g.spb
+        JOIN omd i on i.oms = h.oms
+        WHERE g.spb = '$spb'
+        GROUP BY g.inv,	g.loc, g.remark, g.spesifikasi, g.qty, g.qtygd, g.unitgd, g.cct, h.oms, i.price");
+    }
+
+    function get_detail_data_po($po)  {
+        return DB::connection('mysql')->select("SELECT
+            e.oms as po,
+            e.inv as kode_item,
+            e.loc,
+            f.name as nama_gud,
+            e.remark as nama_item,
+            e.spesifikasi,
+            e.qty as qty_po,
+            e.price,
+            e.val,
+            e.no
+        from omd e
+            JOIN loc f on f.loc = e.loc
+        WHERE e.oms = '$po'");
+    }
 }
